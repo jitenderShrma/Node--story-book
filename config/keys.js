5f05e9ab6ba1d5415922f640efea9e17f8870975
jitenderKumar97 <jitenderkanti020@gmail.com>
2019-02-24 19:44:17 +0530

    final deploy with env varibles

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,5 @@
-// if(process.env.NODE_ENV === 'production'){
-//     module.exports = require('./keys_prod');
-// } else {
-//     module.exports = require('./keys_dev');
-// }
-
-module.exports={
-    mongoURI:'mongodb://jitender:jitender1@ds349455.mlab.com:49455/storybook_db',
-    googleClientID:'548959898258-9hd7jrkf5kmn2ah30bai241ovbmhabtq.apps.googleusercontent.com',
-    googleClientSecret:'MWvXyy8XBD2a89R0i8CSKvgw'
-  }
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
